e6d07665d29925921ebfaf20c66d5f535ccbc3ba
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-02-27 21:52:41 +0100

    Lesson 3: How Inheritance work

--- Ruby_2/Class_Inheritance/index.rb
+++ Ruby_2/Class_Inheritance/index.rb
@@ -41,7 +41,7 @@ food1 = Food.new(name: "Pizza", price: 8)
 puts food1.info
 
 # Assign an instance of Drink with the stated arguments to the drink1 variable
-drink1 = Drink(name: "Cola", price: 3)
+drink1 = Drink.new(name: "Cola", price: 3)
 
 # Print the return value of info method of the drink1 instance
 puts drink1.info
